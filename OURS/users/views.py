918702116a906aecfdff48558aa78b974b915411
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-02 17:20:21 +0100

    Added Auth

--- OURS/users/views.py
+++ OURS/users/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
 
 # Create your views here.
 
 def sign_up(req):
-    form = forms.UserSignupForm()
-    context = {"form": form}
-    return render(req, 'auth/signup.html', context)
+    if req.method == "POST":
+        new_user = forms.UserSignupForm(req.POST)
+        if new_user.is_valid():
+            new_user.save()
+            return redirect('log_in')
+    else:
+        form = forms.UserSignupForm()
+        context = {"form": form}
+        return render(req, 'auth/signup.html', context)
 
 
 def log_in(req):
