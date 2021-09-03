a810395f0ca16328bddd653f8b2e2ef3824bac19
James Derrick <jediblob@aol.com>
2021-09-03 10:03:50 +0100

    add basic dashboard page

--- OURS/main/views.py
+++ OURS/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 # Create your views here.
 
 def homepage(req):
     return render(req, 'pages/homepage.html')
+
+@login_required
+def dashboard(req):
+    return render(req, 'pages/dashboard.html')
