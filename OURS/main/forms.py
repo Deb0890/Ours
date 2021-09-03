0d4dd52c2da7803df00f8c02d9c9c43dbbc5c24c
James Derrick <jediblob@aol.com>
2021-09-03 16:29:56 +0100

    add day selector to new lesson form

--- OURS/main/forms.py
+++ OURS/main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms.widgets import HiddenInput 
+from django.forms.widgets import HiddenInput
 from .models import Lesson, Skill
 
 class NewItemForm(forms.ModelForm):
@@ -8,7 +8,25 @@ class NewItemForm(forms.ModelForm):
         fields = ['name','category']
 
 class NewLessonForm(forms.ModelForm):
+
+    day_choices = ( ("mon", "Monday"), 
+                    ("tue", "Tuesday"), 
+                    ("wed", "Wednesday"), 
+                    ("thu", "Thursday"),
+                    ("fri", "Friday"),
+                    ("sat", "Saturday"),
+                    ("sun", "Sunday")
+                )
+    day_selector = forms.MultipleChoiceField(
+                                            choices=day_choices,
+                                            widget=forms.CheckboxSelectMultiple(),
+                                            label="Available Teaching Days"
+                                            )
+
     class Meta:
         model = Lesson
-        fields = ['tutor','skill','skill_level','title','description','banner_img']
-        widgets = { 'tutor': forms.HiddenInput }
+        fields = ['tutor','skill','skill_level','title','description','banner_img','days','day_selector']
+        widgets = { 
+                    'tutor': forms.HiddenInput,
+                    'days':forms.HiddenInput 
+                }
