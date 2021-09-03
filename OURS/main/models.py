2329a66fe4566cb1c46aa054d26e84beb2023dde
James Derrick <jediblob@aol.com>
2021-09-03 09:20:19 +0100

    add category and skill models and admin forms

--- OURS/main/models.py
+++ OURS/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} ({self.cat.name})'
