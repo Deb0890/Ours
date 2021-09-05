60d7b5cf8f16f6fffcbc34dad5ab15312522c0c7
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-05 14:58:44 +0100

    Add find-a-lesson page & pagination

--- OURS/main/views.py
+++ OURS/main/views.py
@@ -1,6 +1,8 @@
+from django.core import paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import Lesson
 from .forms import NewLessonForm
@@ -37,3 +39,13 @@ def lesson_detail_page(req,id):
     single_lesson = get_object_or_404(Lesson, pk=id)
     context = {"lesson":single_lesson}
     return render(req, 'pages/lesson-single.html', context)
+
+@login_required
+def find_a_lesson(req):
+    lessons = Lesson.objects.order_by('-created')
+    paginator = Paginator(lessons, 2)
+    page_number = req.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {"page_obj": page_obj}
+    return render(req, 'pages/find-a-lesson.html', context)
