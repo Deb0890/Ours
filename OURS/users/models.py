ca1d72405a9eaf65fb1735800e41d74b56413b82
James Derrick <jediblob@aol.com>
2021-09-06 13:16:43 +0100

    add ability to upload images to amazon s3 + profile page

--- OURS/users/models.py
+++ OURS/users/models.py
@@ -8,8 +8,8 @@ from PIL import Image
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_img = models.ImageField(upload_to='users',blank=True)
-    bio = models.TextField(max_length=500, blank=True)
+    profile_img = models.ImageField(upload_to='images/',null=True, blank=True)
+    bio = models.TextField(max_length=500, null=True, blank=True)
     rating = models.PositiveIntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)])
     dollours = models.PositiveIntegerField(default=0)
 
