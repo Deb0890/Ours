b218244fffcc0570039a9c8dcb3c2b26906e6788
James Derrick <jediblob@aol.com>
2021-09-06 10:44:05 +0100

    add profile model

--- OURS/users/forms.py
+++ OURS/users/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import widgets
+from .models import Profile
 
 class UserSignupForm(UserCreationForm):
-    # email = forms.EmailField()
-    # first_name = forms.CharField(max_length=50)
-    # last_name = forms.CharField(max_length=50)
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_img','bio')
