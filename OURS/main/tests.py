1ebe85b2b417c812b76cea09a2bae31e3bccf726
deb0890 <deborahf1@hotmail.com>
2021-09-07 12:59:42 +0100

    add setup for model tests

--- OURS/main/tests.py
+++ OURS/main/tests.py
@@ -1,3 +1,4 @@
+from main.models import Lesson, Category, Skill, SkillLevels
 from io import StringIO
 from django.http import response
 from django.test import TestCase, Client
@@ -24,6 +25,30 @@ class BaseTestCase(TestCase):
         }
 
         cls.users_info = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
+        cls.users_info.save()
+        cls.category = Category.objects.create(name='music')
+        cls.category.save()
+        cls.skill = Skill.objects.create(name='violin', category=cls.category)
+        cls.skill.save()
+
+        cls.example_lesson = Lesson.objects.create(
+            tutor = cls.users_info,
+            skill = cls.skill,
+            skill_level = 'beginner',
+            title = 'Learn music here!',
+            description = 'lesson description',
+            banner_img = 'tbc',
+            days = 'Monday',
+            created = 'tbc'
+        )
+
+class LessonModelTests(BaseTestCase):
+
+    def test_items_in_db(self):
+        lesson = Lesson.objects.all()
+        print(lesson)
+        self.assertFalse()
+
 
 class PagesUrlTests(BaseTestCase):
     def setUp(self):
@@ -44,8 +69,7 @@ class PagesUrlTests(BaseTestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_lesson_single_response_status(self):
-        response = self.client.get(reverse('lesson-single'), follow=True)
-        print(response)
+        response = self.client.get(reverse('lesson-single', args=[1]), follow=True)
         self.assertEqual(response.status_code, 200)
     
     def test_find_a_lesson_response_status(self):
@@ -54,35 +78,6 @@ class PagesUrlTests(BaseTestCase):
 
     
     
-#     def test_content_of_title_is_signup(self):
-#         response = self.client.get(reverse('sign_up'), follow=True)
-#         html = response.content
-#         html_in_text_format = BeautifulSoup(html, 'html.parser')
-#         assert html_in_text_format.title.string == 'Sign Up'
-    
-#     def test_content_of_title_is_login(self):
-#         response = self.client.get(reverse('log_in'), follow=True)
-#         html = response.content
-#         html_in_text_format = BeautifulSoup(html, 'html.parser')
-#         assert html_in_text_format.title.string == 'Log In'
-
-#     #signup page should redirect on submitting
-#     def test_signup_page_post_response(self):
-#         response = self.client.post(reverse('sign_up'), self.user, follow=True)
-#         self.assertTemplateUsed(response,'pages/dashboard.html')
-
-#     #also possibly create test that shouldn't sign up user with an existing username, 409?
-
-#     def test_the_right_template_is_rendered(self):       
-#         response = self.client.post(reverse('sign_up'), self.user, follow=True)
-#         # create a list of templates and check the one we want to use is in that list
-#         assert('auth/signup.html' not in [template.name for template in response.templates])
-
-#     def test_base_template_present(self):
-#         response = self.client.get(reverse('sign_up'), follow=True)
-#         #templates = [template for template in response.templates]
-#         assert('auth/base.html' in [template.name for template in response.templates])
-
         
     #correct page renders
     def test_homepage_renders(self):
@@ -101,22 +96,6 @@ class PagesUrlTests(BaseTestCase):
         response = self.client.get(reverse('find-a-lesson'), follow=True)
         self.assertTemplateUsed(response, 'pages/find-a-lesson.html')
 
-# class ProtectedRoutesTests(BaseTestCase):
-
-#     def setUp(self):
-#         self.client = Client()
-#         self.client.login(username= 'myusername', password= 'mypassword')
-
-#     def test_can_view_dashboard_when_logged_in(self):
-#         response = self.client.get(reverse('dashboard'), follow=True)
-#         #self.assertTemplateNotUsed('pages/dashboard.html')
-#         assert('pages/dashboard.html' in [template.name for template in response.templates])
-   
-#     def test_dashboard_extends_base_template(self):
-#         response = self.client.get(reverse('dashboard'), follow=True)
-#         #self.assertTemplateNotUsed('pages/dashboard.html')
-#         assert('pages/base.html' in [template.name for template in response.templates])
-
 class ErrorRoutes(BaseTestCase):
 
 
@@ -133,12 +112,3 @@ class ErrorRoutes(BaseTestCase):
     def test_view_url_does_not_exist(self):
         response = self.client.get('/home', follow=True)
         self.assertEqual(response.status_code, 404)
-
-
-# class Test_tests(TestCase):
-
-#     def test_one_one(self):
-#         self.assertEqual(1+1, 2)
-
-#     def test_one_Two(self):
-#         self.assertEqual(1+1, 3)
