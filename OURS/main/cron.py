8bf4da6391120d7b64708b24995e86b27ff1b2b3
James Derrick <jediblob@aol.com>
2021-09-08 17:03:28 +0100

    handle review voids

--- OURS/main/cron.py
+++ OURS/main/cron.py
@@ -12,18 +12,15 @@ def close_rooms_cron_job():
     classrooms = Classroom.objects.filter(state="FR")
     for classroom in classrooms:
         review = Review.objects.filter(id=classroom.id)
-        if review.tutor_void and review.student_void:
-            return
-        else:
-            student = User.objects.filter(id=classroom.student.id)    
-            tutor = User.objects.filter(id=classroom.skill.tutor.id)
-            student.profile.classroom_complete += 1
-            student.profile.rating += review.tutor_review_score
-            student.profile.dollours -= 1
-            tutor.profile.classroom_complete += 1
-            tutor.profile.rating += review.student_review_score
-            tutor.profile.dollours += 1
-            classroom.state = "CL"
-            student.save()
-            tutor.save()
-            classroom.save()    
+        student = User.objects.filter(id=classroom.student.id)    
+        tutor = User.objects.filter(id=classroom.skill.tutor.id)
+        student.profile.classroom_complete += 1
+        student.profile.rating += review.tutor_review_score
+        student.profile.dollours -= 1
+        tutor.profile.classroom_complete += 1
+        tutor.profile.rating += review.student_review_score
+        tutor.profile.dollours += 1
+        classroom.state = "CL"
+        student.save()
+        tutor.save()
+        classroom.save()    
